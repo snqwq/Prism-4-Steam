338440a2a0d6e33dba38251eb0941b6a2b77997f
snqwq <99054951+snqwq@users.noreply.github.com>
2025-06-23 17:13:22 -0500

    begin config file

---
+++ config.py
@@ -0,0 +1,6 @@
+# prism installation paths
+WINDOWS_PATH = "%APPDATA%/PrismLauncher"
+SCOOP_PATH = "%HOMEPATH%\scoop\persist\prismlauncher"
+MACOS_PATH = "~/Library/Application Support/PrismLauncher"
+LINUX_PATH = "~/.local/share/PrismLauncher"
+FLATPAK_PATH = "~/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher"
