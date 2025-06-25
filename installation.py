d5a7267f5f92d43fd071a826f719c936a9fa78c7
snqwq <99054951+snqwq@users.noreply.github.com>
2025-06-24 22:11:22 -0500

    guys look i am doing stuff that isnt just readme updates i swear

--- installation.py
+++ installation.py
@@ -4,19 +4,30 @@ import subprocess
 import config
 import platform
 import pathlib
+import json
 
 prism_installed = False
+install_path = ""
 
 # system checking
 if platform.system() == "Windows":
     if pathlib.Path(config.WINDOWS_PATH).exists():
         prism_installed = True
+        install_path = config.WINDOWS_PATH
+        print("prism installed")
+
     elif pathlib.Path(config.SCOOP_PATH).exists():
         prism_installed = True
+        print("prism installed with Scoop")
+
     else:
         print("cannot find prism install")
-    print(f"is prism installed? {prism_installed}")
+
 elif platform.system() == "Linux":  # TODO add linux support
-    print("not yet supported because im lazy")
+    print("linux not yet supported because im lazy")
+
 else:
     print("operating system unknown, or we dont support it yet")
+
+# grab all instances
+with open()
